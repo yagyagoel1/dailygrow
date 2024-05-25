8b655a8ef0ed48060a0a54b4d4cfef264a4acdfc
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-25 23:32:03 +0530

    initialized

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+
+import app from './app.js'
+
+dotenv.config({path: '../.env'})
+
+try{
+    app.on('error', (error) => {
+        throw error;
+
+    })
+    app.listen(process.env.PORT||3000, () => {
+        console.log(`Server is running on port: ${process.env.PORT}`)
+    })
+}
+catch(error){
+    console.log(`Something went wrong while connecting express: ${error.message}`)
+}
---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+const asyncHandler = (fn) => async(req, res, next) =>{
+    try {
+        fn(req, res, next)
+    } catch (error) {
+        console.log(`Something went wrong while connecting express: ${error.message}`)
+        res.staus(500).json({message: 'Internal server error'})
+    }
+}
