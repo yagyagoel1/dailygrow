22b3193d1bb612c0673b19a2496c64bec3482738
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-26 22:19:55 +0530

    fixed the bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ try{
 
     })
     app.listen(process.env.PORT||3000, () => {
-        console.log(`Server is running on port: ${process.env.PORT}`)
+        console.log(`Server is running on port: ${process.env.PORT||3000}`)
     })
 }
 catch(error){
